eb959e87fc7527ee995fc3ad9b9b176bc8011842
Juan Francisco Rodriguez <juanfrh7@icloud.com>
2023-06-13 12:25:09 +0100

    add description to each function

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,9 @@
-# Find composite index into 1D list for (generator, hour)
 def get_index(generator_index, hour_index, n_hours) -> int:
+    """# Find composite index into 1D list for (generator, hour)"""
     return generator_index * n_hours + hour_index
 
-# Inverse of get_index - given a composite index in a 1D list, return the
-# generator and hour
 def get_generator_and_day(index, hours) -> tuple:
+    """Inverse of get_index - given a composite index in a 1D list 
+    Return the generator and hour"""
     generator_index, hour_index = divmod(index, hours)
     return generator_index, hour_index
